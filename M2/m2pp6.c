74121963923175bf497d3b41279409b4a75a83c3
Rayat Al Islam <thenvmeguy@gmail.com>
2026-07-01 04:02:20 +0600

    optimized pp6

--- M2/m2pp6.c
+++ M2/m2pp6.c
@@ -10,57 +10,51 @@ int main()
     printf("5. Dhaka \n");
     printf("    Enter Your Destination(1-5_): ");
     scanf("%d", &choice);
-    printf("How many adult: ");
+    printf("How many adult: \n");
     scanf("%d", &adult);
-    printf("\nHow many child: ");
+    printf("How many child: ");
     scanf("%d", &child);
+    printf("\n**** Your Ticket ****\n");
+    printf("Starting Station: Rajshahi\n");
     switch (choice)
     {
         case 1: 
         {
             fare = adult*150*1 + child*75*1;
-            printf("**** Your Ticket ****\n");
-            printf("Starting Station: Rajshahi\n");
+            
             printf("Destination Station: Ishwardi\n");
-            printf("Total Fare: %d", fare);
             break;
         }
         case 2: 
         {
             fare = adult*150*2 + child*75*2;
-            printf("**** Your Ticket ****\n");
-            printf("Starting Station: Rajshahi\n");
+            
             printf("Destination Station: Ullapara\n");
-            printf("Total Fare: %d", fare);
             break;
         }
         case 3: 
         {
             fare = adult*150*3 + child*75*3;
-            printf("**** Your Ticket ****\n");
-            printf("Starting Station: Rajshahi\n");
+            
             printf("Destination Station: Tangail\n");
-            printf("Total Fare: %d", fare);
             break;
         }
         case 4: 
         {
             fare = adult*150*4 + child*75*4;
-            printf("**** Your Ticket ****\n");
-            printf("Starting Station: Rajshahi\n");
+            
             printf("Destination Station: Jaydebpur\n");
-            printf("Total Fare: %d", fare);
             break;
         }
         case 5: 
         {
             fare = adult*150*5 + child*75*5;
-            printf("**** Your Ticket ****\n");
-            printf("Starting Station: Rajshahi\n");
+            
             printf("Destination Station: Dhaka\n");
-            printf("Total Fare: %d \n", fare);
             break;
         }
     }
+    printf("Total Fare: %d \n", fare);
+    printf("**** Bangladesh Railways ****\n");
 
 }
