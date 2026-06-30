ae0ba2af62d47d1f8f893f589034ba82dde575d6
Rayat Al Islam <thenvmeguy@gmail.com>
2026-06-30 20:02:31 +0600

    few codes from module 1,2,3 are added

---
+++ M1PP7.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    /* my first program */
+    float a, b, c, d, e, f, area ;
+    printf("Enter Three Vertices (enter in pairs, separated by space)\n");
+    scanf("%f %f", &a, &b);
+    scanf("%f %f", &c, &d);
+    scanf("%f %f", &e, &f);
+    area = 0.5*(a*d + c*f + e*b -b*c -d*e - f*a);
+    area = fabs(area);
+    printf("The area is: %f \n", area);
+    return 0;
+
+
+
+}
